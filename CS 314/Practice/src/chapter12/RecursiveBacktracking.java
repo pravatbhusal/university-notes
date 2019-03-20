55715d95d399c8ff9402deb9857399ab520fc468
Pravat Bhusal <pravat.bhusal@gmail.com>
2019-03-20 00:08:25 -0500

    Add the ways to climb backtracking solution

--- CS 314/Practice/src/chapter12/RecursiveBacktracking.java
+++ CS 314/Practice/src/chapter12/RecursiveBacktracking.java
@@ -20,9 +20,27 @@ public class RecursiveBacktracking {
 				so print the current number that contains all the concatenated 0s and 1s */
 			System.out.println(currentNumber);
 		} else {
-			// create cases for when the next binary is 0 or 1
+			// do every binary combination of either 0 or 1, becoming O(N^2)
 			helperCountBinary(number - 1, currentNumber + "0");
 			helperCountBinary(number - 1, currentNumber + "1");
+		}
+	}
+
+	public static void waysToClimb(int stairs) {
+		helperWaysToClimb(stairs, 0, "[");
+	}
+
+	// a helper method for the ways to climb method
+	private static void helperWaysToClimb(int stairs, int stepsSum, String steps) {
+		if(stepsSum == stairs && stairs != 0) {
+			// base-case for this recursive call combination, print the steps Stack without trailing comma
+			final int TRAILING_COMMA_LENGTH = 2;
+			steps = steps.substring(0, steps.length() - TRAILING_COMMA_LENGTH);
+			System.out.println(steps + "]");
+		} else if(stepsSum < stairs) {
+			// test out every stairs combination of either 1 or 2, becoming O(N^2)
+			helperWaysToClimb(stairs, stepsSum + 1, steps + "1, ");
+			helperWaysToClimb(stairs, stepsSum + 2, steps + "2, ");
 		}
 	}
 }
