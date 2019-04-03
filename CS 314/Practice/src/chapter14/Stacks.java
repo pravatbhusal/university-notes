764677a4c6e6d694d818d2d12b20ed65c60a4578
Pravat Bhusal <pravat.bhusal@gmail.com>
2019-04-03 15:54:18 -0500

    Add the is syntax valid practice for paranthesis

--- CS 314/Practice/src/chapter14/Stacks.java
+++ CS 314/Practice/src/chapter14/Stacks.java
@@ -26,4 +26,34 @@ public class Stacks {
 			st.push(value);
 		}
 	}
+	
+		/*
+	 * Check if a parenthesis syntax is valid (balanced).
+	 * pre (Assume implemented): All characters are either '(' or ')'
+	 * Ex: (())(()) is balanced
+	 * Ex: ())() is not balanced
+	 */
+	public static boolean isSyntaxValid(String syntax) {
+		Stack<Character> syntaxStack = new Stack<Character>();
+
+		// iterate through the syntax String
+		final int SYNTAX_LENGTH = syntax.length();
+		for(int charIndex = 0; charIndex < SYNTAX_LENGTH; charIndex++) {
+			char syntaxChar = syntax.charAt(charIndex);
+
+			if(syntaxChar == '(') {
+				// for an open parenthesis, add to the stack
+				syntaxStack.push(syntaxChar);
+			} else if(syntaxChar == ')' && !syntaxStack.isEmpty()) {
+				// pop whenever encountering a closing parenthesis
+				syntaxStack.pop();
+			} else if(syntaxStack.isEmpty()) {
+				/* the Stack is empty too early, deleted more opening
+					parenthesis than needed or didn't start with one */
+				return false;
+			}
+		}
+		// if there are still parenthesis left-over, then it's invalid
+		return syntaxStack.isEmpty();
+	}
 }
