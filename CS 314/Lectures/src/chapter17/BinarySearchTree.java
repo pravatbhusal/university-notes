dfb70eaf929a7092ca71507913bbffa549724645
Pravat Bhusal <pravat.bhusal@gmail.com>
2019-04-03 15:15:22 -0500

    Finish the implementation of BST
    We may add more things in the future, but for now this is what we discussed in class.

--- CS 314/Lectures/src/chapter17/BinarySearchTree.java
+++ CS 314/Lectures/src/chapter17/BinarySearchTree.java
@@ -21,7 +21,7 @@ public class BinarySearchTree<E extends Comparable<? super E>> {
 		// set the root Node equal to the helper (in-case it's null)
 		root = addHelper(value, root);
 
-		// if the old size has changed, then a value was added
+		// if the old size has changed, then a Node was added
 		return oldSize != size;
 	}
 
@@ -44,6 +44,95 @@ public class BinarySearchTree<E extends Comparable<? super E>> {
 		}
 		// return back the same Node, so we don't change the Node reference
 		return node;
+	}
+
+	// return the height of the BST from the root Node
+	public int height() {
+		return helperHeight(root);
+	}
+
+	// a helper method to recursively get the height of the tree
+	private int helperHeight(BNode<E> node) {
+		if(node == null) {
+			// outside the tree, subtract one because out-of-bounds
+			return -1;
+		}
+		/* traverse through the entire tree, and add-up the greatest
+			height between the right and left sub-trees */
+		return 1 + Math.max(helperHeight(node.rightChild), helperHeight(node.leftChild));
+	}
+
+	// remove a value from the BST, return if successful
+	public boolean remove(E value) {
+		int oldSize = size;
+
+		// set the root Node equal to the helper (in-case it deletes the root)
+		root = removeHelper(value, root);
+
+		// if the old size has changed, then a Node was deleted
+		return oldSize != size;
+	}
+
+	// a helper method to recursively remove a value from the tree
+	private BNode<E> removeHelper(E value, BNode<E> node) {
+		if(node != null) {
+			// determine the direction by comparing the value to this Node's data
+			int direction = value.compareTo(node.data);
+			if(direction < 0) {
+				// target value is less than the Node, so recurse left
+				node.leftChild = removeHelper(value, node.leftChild);
+			} else if(direction > 0) {
+				// target value is greater than the Node, so recurse right
+				node.rightChild = removeHelper(value, node.rightChild);
+			} else {
+				// found the target Node, let's delete it
+				size--;
+				if(node.leftChild == null && node.rightChild == null) {
+					// delete the leaf Node
+					node = null;
+				} else if(node.rightChild == null) {
+					// the Node to delete only has one child, the left child
+					// delete the Node by linking the Node's parent to the Node's left child
+					node = node.leftChild;
+				} else if(node.leftChild == null) {
+					// the Node to delete only has one child, the right child
+					// delete the Node by linking the Node's parent to the Node's right child
+					node = node.rightChild;
+				} else {
+					// the Node has two children, so copy it to the max of the left-subtree
+					node.data = getMaxValue(node.leftChild);
+
+					// now there are duplicates, remove the max of the left-subtree Node
+					/* sooner or later it'll hit the original max of the left
+						subtree and execute the leaf Node deletion case */
+					node.leftChild = removeHelper(node.data, node.leftChild);
+
+					// since it's recursive, we need to backtrack size correctly (GACKY!)
+					/* it'll still decrement by one since there'll be a recursive
+						case where this else statement does not execute */
+					size++;
+				}
+			}
+		}
+		// return back the Node, which may or may not change based on if it was deleted
+		return node;
+	}
+
+	// return the maximum data from a Node sub-tree
+	private E getMaxValue(BNode<E> node) {
+		BNode<E> currentNode = node;
+		E data = null;
+
+		// iterate the right-subtree from this Node to get the maximum value
+		while(currentNode != null) {
+			if(currentNode.rightChild == null) {
+				// the base-case, the next right child Node is null so set the value
+				data = currentNode.data;
+			}
+			// traverse right (greater value)
+			currentNode = currentNode.rightChild;
+		}
+		return data;
 	}
 
 	// print the pre-order traversal of the tree
@@ -117,16 +206,31 @@ public class BinarySearchTree<E extends Comparable<? super E>> {
 	// a tester method for the BST
 	public static void main(String[] args) {
 		BinarySearchTree<Integer> bTree = new BinarySearchTree<>();
+
+		// add
 		bTree.add(12);
 		bTree.add(49);
 		bTree.add(42);
 		bTree.add(13);
 		bTree.add(5);
+
+		// traversals
 		System.out.println("Pre-Order Traversal");
 		bTree.printPreOrder();
 		System.out.println("In-Order Traversal");
 		bTree.printInOrder();
 		System.out.println("Post-Order Traversal");
 		bTree.printPostOrder();
+		System.out.println();
+
+		// height
+		System.out.println("The height is: " + bTree.height());
+		System.out.println();
+
+		// remove
+		bTree.remove(5);
+		bTree.remove(13);
+		System.out.println("Pre-Order Traversal to Test Remove");
+		bTree.printPreOrder();
 	}
 }
