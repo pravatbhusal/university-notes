28a27722dfa4285210b97a256b25f10b1a9893fa
Pravat Bhusal <pravat.bhusal@gmail.com>
2019-04-09 22:39:04 -0500

    Explanation on iterating through a Queue without modifying it

--- CS 314/Practice/src/chapter14/Queues.java
+++ CS 314/Practice/src/chapter14/Queues.java
@@ -3,11 +3,21 @@ package chapter14;
 import java.util.Queue;
 
 public class Queues {
-
+	
 	/*
 	 * Determine if the elements in an Integer Queue are strictly decreasing from front to back.
 	 * pre: All elements in the queue > 0
 	 * post: The queue is restored to its original state, may not use an auxiliary data structure.
+	 * 
+	 * How to iterate through the Queue without modifying it:
+	 * Let's say the Queue starts as [12, 10, 8, 8, 7]
+	 * - We add a 0 flag, so it's now [12, 10, 8, 8, 7, 0]
+	 * - Then dequeue (remove) and enqueue the front to the back [10, 8, 8, 7, 0, 12]
+	 * - Then do the same step again, so dequeue, [8, 8, 7, 0, 12, 10]
+	 * - Keep going... [8, 7, 0, 12, 10, 8] -> [7, 0, 12, 10, 8, 8] -> [0, 12, 10, 8, 8, 7]
+	 * - Now this is the element at the flag, so remove it [12, 10, 8, 8, 7] however the loop
+	 * 		will not run again so it won't be enqueued at the back of the Queue
+	 * - Therefore, we have iterated through the entire Queue without modifying it.
 	 */
 	public static boolean isStrictlyDecreasing(Queue<Integer> queue) {
 		queue.add(0); // flag, helps us determine stop point
@@ -21,8 +31,8 @@ public class Queues {
 				// if the previous is greater, then the queue is decreasing
 				decreasing = prev > current;
 			}
-			// push the list
-			queue.remove();
+			// push the previous element to the back of the queue
+			queue.add(prev);
 			prev = current;
 		}
 		return decreasing;
