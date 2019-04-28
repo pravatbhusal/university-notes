7041f6a8cf1e7a42ec306639758971ca6e0a7283
Pravat Bhusal <pravat.bhusal@gmail.com>
2019-04-28 17:55:53 -0500

    Fix typo for Queue

--- CS 314/Lectures/src/chapter14/Queue.java
+++ CS 314/Lectures/src/chapter14/Queue.java
@@ -11,7 +11,7 @@ public class Queue<E> {
 	// last item added to the queue
 	private int last;
 
-	// current queue of the queue
+	// current size of the queue
 	private int size;
 
 	// constructor, initialize the queue container
